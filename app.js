7d00c1807d854889209f8f457ebd2a3aff74cabc
David Rosen <david.tzvi.rosen@gmail.com>
2020-12-17 20:35:11 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+// const mongoose = require("mongoose");
+// const path = require("path");
+// const bodyParser = require("body-parser");
+// const dotenv = require("dotenv");
+
+// dotenv.config();
+
+const app = express();
+// app.use(bodyParser.json());
+// app.use(cors());
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
---
+++ client/src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "../css/styles.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
