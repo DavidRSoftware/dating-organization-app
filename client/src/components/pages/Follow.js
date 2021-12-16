e7b0af1e89376c173bf2bb448dc4fd1008056508
David <David>
2021-12-15 21:55:40 -0500

    update

--- client/src/components/pages/Follow.js
+++ client/src/components/pages/Follow.js
@@ -1,23 +1,24 @@
 import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 const Follow = () => {
     return (
         <div>
             <h1>Follow Up</h1>
             <h2>Select a Event</h2>
-            <select name="" id="">
+            <Form.Select aria-label="Select Event">
                 <option value="">Select Event</option>
                 <option value="">Local Restaurant</option>
                 <option value="">Casual Meetup</option>
-            </select>
+            </Form.Select>
             <h2>Select a person who you would like to comment about</h2>
-            <select name="" id="">
+            <Form.Select aria-label="Select Person">
                 <option value="">Select Person</option>
                 <option value="">Sarah Roberts</option>
                 <option value="">Leah Samuels</option>
-            </select>
+            </Form.Select>
             <div>
-            <Button variant="secondary">Next</Button>
+                <Button variant="secondary">Next</Button>
             </div>
         </div>
     )
